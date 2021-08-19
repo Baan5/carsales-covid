673b8e2b31d0fdb1ac170c46ac1e8e28913c9db7
oscaredu <oalvarez.torneria@gmail.com>
2021-08-19 19:34:29 -0400

    Splash Screen y dependencias

---
+++ app/src/main/java/com/bangover/carsalescovid/InfoCovid.kt
@@ -0,0 +1,11 @@
+package com.bangover.carsalescovid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfoCovid : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_covid)
+    }
+}
--- app/src/main/java/com/bangover/carsalescovid/MainActivity.kt
+++ app/src/main/java/com/bangover/carsalescovid/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.bangover.carsalescovid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, InfoCovid::class.java)
+            startActivity(intent)
+        }, 3000)
+
     }
 }
