642ca491eab974571339dc43190a6e76a9e91ceb
Oscar Alvarez <oscar.alvarez@carsales.com>
2021-09-10 11:09:58 -0300

    Se establece fecha minima y maxima para seleccionar

--- app/src/main/java/com/bangover/carsalescovid/Core/DatePickerFragment.kt
+++ app/src/main/java/com/bangover/carsalescovid/Core/DatePickerFragment.kt
@@ -6,6 +6,8 @@ import android.content.Context
 import android.os.Bundle
 import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
+import java.text.ParseException
+import java.text.SimpleDateFormat
 import java.util.*
 
 class DatePickerFragment(val listener: (day: Int, month: Int, year: Int) -> Unit) :
@@ -15,11 +17,22 @@ class DatePickerFragment(val listener: (day: Int, month: Int, year: Int) -> Unit
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        var timeInMillis: Long = 0
+        val sdf = SimpleDateFormat("yyyy MM dd")
+        try {
+            val mDate: Date = sdf.parse("2020 01 22")
+            timeInMillis = mDate.time
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
         val picker = DatePickerDialog(activity as Context, this, year, month, day)
+        picker.datePicker.minDate = timeInMillis
+        picker.datePicker.maxDate = c.timeInMillis
         return picker
     }
 
